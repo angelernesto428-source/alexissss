25c4685ee2b187b492a24a5baae1571bc7a8f66c
angelernesto428-source <angelernesto428@gmail.com>
2025-08-30 16:23:39 -0400

    Add files via upload

---
+++ hola.py
@@ -0,0 +1,6 @@
+edad=17
+nombre="jdncsb"
+print("yo soy " + nombre + " y tengo " + str (edad) + " años")
+print(f"yo soy {nombre} y tengo {edad} años.")
+
+print("nose que poner auyuyuyiuui invitame marito")
