e7c8885846068f95494a3f1e98824783b37923ec
maluco <angelernesto428@gmail.com>
2025-09-14 19:42:32 -0400

    Add files via upload

---
+++ examen phyton/clasificaciones individual.py
@@ -0,0 +1,40 @@
+#coas de clasificacion
+
+
+registros_estudiantes = [
+    ('Juan Pénez', 'Matemáticas', 85, 92),
+    ('María Lopez', 'Historia', 65, 88),
+    ('Carlos Garcia', 'Ciencias', 96, 99),
+    ('Ana Torres', 'Literatura', 92, 75), 
+    ('Pedro Ramirez', 'Arte', 70, 81),
+    ('Sofia Mendoza', 'Química', 98, 97)
+]
+
+
+aprobados = []
+reprobados = []
+cuadro_de_honor = []
+
+
+for estudiante in registros_estudiantes:
+    nombre, materia, calificacion, asistencia = estudiante
+
+  
+    if asistencia < 80:
+        reprobados.append(nombre)
+    
+    elif calificacion < 70:
+        reprobados.append(nombre)
+   
+    elif calificacion >= 95:
+        cuadro_de_honor.append(nombre)
+        aprobados.append(nombre)
+    
+    else:
+        aprobados.append(nombre)
+
+
+print("--- Reporte de Clasificación Académica ---")
+print(f" Aprobados: {aprobados}")
+print(f" Reprobados: {reprobados}")
+print(f"Cuadro de Honor: {cuadro_de_honor}")
